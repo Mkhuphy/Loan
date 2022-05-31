c0c3961681b471b2ff7f3338aa40db0b8f5a4377
Mkhuphy <105905301+Mkhuphy@users.noreply.github.com>
2022-05-31 17:40:16 +0530

    Add files via upload

---
+++ Assignment 1/web.py
@@ -0,0 +1,29 @@
+import hashlib
+
+i = 0
+c = 0
+
+
+def hexa(st2):
+    hex = 0
+
+    for i in range(len(st2)):
+        stval = st2[i]
+        if (ord(stval) == 48):
+            hex += 1
+        else:
+            break
+    return (hex)
+
+
+while (c == 0):
+    st = "iitk" + str(i)
+    i += 1
+    st1 = hashlib.sha256(st.encode('utf-8')).hexdigest()
+    vst1 = hexa(st1)
+
+    if vst1 >= 4:
+        c += 1
+
+print(i - 1)
+
