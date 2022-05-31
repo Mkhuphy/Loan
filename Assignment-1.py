9a34fba918f4f6b7d382d68e85054a8773b0b7ef
Mkhuphy <105905301+Mkhuphy@users.noreply.github.com>
2022-05-31 17:00:30 +0530

    Update Assignment-1.py

--- Assignment-1.py
+++ Assignment-1.py
@@ -7,18 +7,15 @@ c = 0
 
 def hexa(st2):
     hex = 0
-    p = 1
+   
 
-    l = len(st2)
+    
     for i in range(len(st2)):
-        stval = st2[l - i - 1]
-        if (ord(stval) < 60):
-            val = ord(stval) - 48
+        stval = st2[ i ]
+        if (ord(stval) == 48):
+            hex += 1
         else:
-            stval.lower()
-            val = ord(stval) - 87
-        hex += p * val
-        p *= 16
+          break
     return (hex)
 
 
@@ -27,8 +24,8 @@ while (c == 0):
     i += 1
     st1 = hashlib.sha256(st.encode('utf-8')).hexdigest()
     vst1 = hexa(st1)
-    hex1 = hexa("0000"+("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF").lower())
-    if vst1 < hex1:
+    
+    if vst1 >= 5:
         c += 1
 
 print(i-1)
